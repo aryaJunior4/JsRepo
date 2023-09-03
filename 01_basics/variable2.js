b2712fafa734f6ac82e583d4d2e3516a78f468de
aryaJunior4 <97622540+aryaJunior4@users.noreply.github.com>
2023-09-03 18:30:39 +0000

    datatypes

---
+++ 01_basics/variable2.js
@@ -0,0 +1,24 @@
+"use strict";// treat all js code as newer version
+
+// alert("hello") // we are using node js not browser 
+
+
+
+let name = "Shashank"
+let age = 24
+let isLoggedIn = "true"
+
+
+// number = 2 to pwr 53
+// bigint 
+// string ""
+// boolean true or false
+// null  standalone value
+// undefined 
+// symbol - unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
