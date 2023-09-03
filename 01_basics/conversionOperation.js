a45ea12ea67d1d598f97ba77eafb3080d2c355ea
aryaJunior4 <97622540+aryaJunior4@users.noreply.github.com>
2023-09-03 19:00:00 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,51 @@
+let age = "100abc"
+
+// console.log(typeof age);
+
+let valueInNumbers = Number(age)
+// console.log(typeof valueInNumbers);
+// console.log(valueInNumbers);
+
+
+// "33" -- 33
+// "33abc" -- NaN
+// true = 1 , false = 0
+
+let isLoggedIn = 1
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Shashank" => true
+
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+// console.log(StringNumber);
+// console.log(typeof(StringNumber));
+
+//************************OPERATIONS*********************************** */
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+//console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 5;
+
+
+let counter = 100
+counter++
+console.log(counter);
