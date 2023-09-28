4bc649453ac969f0fe9e0618288cbc8b4c5fbbf0
aryaJunior4 <97622540+aryaJunior4@users.noreply.github.com>
2023-09-29 00:10:25 +0530

    final commit

---
+++ instaLike/script.js
@@ -0,0 +1,19 @@
+let con = document.querySelector("#container");
+let dil = document.querySelector("i");
+
+con.addEventListener("dblclick", function () {
+        dil.style.transform = 'translate(-50%,-50%) scale(1)';
+        dil.style.opacity = 0.8;
+
+        setTimeout(function () {
+            dil.style.opacity = 0;
+        }, 1000);
+
+        setTimeout(function () {
+            dil.style.transform = 'translate(-50%,-50%) scale(0)';
+        }, 1000);
+        
+});
+
+
+
